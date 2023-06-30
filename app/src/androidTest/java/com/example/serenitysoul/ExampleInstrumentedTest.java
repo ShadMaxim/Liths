d752992c6da705e51f1da57747447645f1550c04
Shadchnev Maxim <maksim.shadchnev@alseda.by>
2023-06-30 09:51:18 +0300

    add junit implementation in gradle

--- app/src/androidTest/java/com/example/serenitysoul/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/serenitysoul/ExampleInstrumentedTest.java
@@ -3,7 +3,7 @@ package com.example.serenitysoul;
 import android.content.Context;
 
 import androidx.test.InstrumentationRegistry;
-import androidx.test.runner.AndroidJUnit4;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.testtask", appContext.getPackageName());
+        assertEquals("com.example.serenitysoul", appContext.getPackageName());
     }
 }
